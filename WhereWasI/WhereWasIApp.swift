20086495f382540e96ca027920a43462b860e249
ced320 <77014600+ced320@users.noreply.github.com>
2024-07-24 18:58:36 +0200

    Initial Commit

---
+++ WhereWasI/WhereWasIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereWasIApp.swift
+//  WhereWasI
+//
+//  Created by Cedric Frimmel-Hoffmann on 24.07.24.
+//
+
+import SwiftUI
+
+@main
+struct WhereWasIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
